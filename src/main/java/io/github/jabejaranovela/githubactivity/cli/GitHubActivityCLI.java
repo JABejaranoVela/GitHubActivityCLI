0d9aba8329f7e440225fa2812c1e6a2a2a81b01c
Jose Antonio Bejarano Vela <joseantoniobejarano@outlook.es>
2025-12-09 22:30:15 +0100

    Primer commit

---
+++ src/main/java/io/github/jabejaranovela/githubactivity/cli/GitHubActivityCLI.java
@@ -0,0 +1,7 @@
+package io.github.jabejaranovela.githubactivity.cli;
+
+public class GitHubActivityCLI {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHubActivityCLI!");
+    }
+}
